511ac4f2a2fb148cfd9a189f7a123b7730428686
Chandler <escalachandler@gmail.com>
2023-12-20 01:22:53 +0800

    Validation Final

--- Dbsys/Forms/Frm_Customer_Sales.cs
+++ Dbsys/Forms/Frm_Customer_Sales.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
@@ -71,18 +72,48 @@ namespace Dbsys
                 cbSeatNumber.DataSource = availableSeats;
             }
         }
+        public bool validation()
+        {
+            // Validate inputs
+            if (string.IsNullOrWhiteSpace(txtCustomerName.Text) || string.IsNullOrWhiteSpace(txtQuantity.Text) || cbSeatNumber.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please fill in all required fields and choose a seat.");
+                return false;
+            }
 
+            if (!int.TryParse(txtQuantity.Text, out int quantity) || quantity <= 0)
+            {
+                MessageBox.Show("Invalid ticket quantity. Please enter a whole number.");
+                return false;
+            }
+
+            if (!decimal.TryParse(txtPaidAmount.Text, out decimal paidAmount) || paidAmount <= 0)
+            {
+                throw new ValidationException("Invalid paid amount. Please enter a valid non-negative amount.");
+            }
+
+            decimal totalAmount = quantity * selectedShowtime.Movie.moviePrice;
+            if (decimal.Parse(txtPaidAmount.Text) < totalAmount)
+            {
+                MessageBox.Show("Insufficient paid amount. Please enter an amount equal to or greater than the total amount.");
+                return false;
+            }
+
+            return true;
+
+        }
+       
         private void CalculateTotalAmount()
         {
-            // Calculate total amount based on ticket price and quantity
-            if (int.TryParse(txtQuantity.Text, out int quantity))
-            {
-                decimal ticketPrice = selectedShowtime.Movie.moviePrice;
-                decimal totalAmount = quantity * ticketPrice;
-                lblTotalAmount.Text = $"Total Amount: Php {totalAmount:F2}";
-            }
+                // Calculate total amount based on ticket price and quantity
+                if (int.TryParse(txtQuantity.Text, out int quantity))
+                {
+                    decimal ticketPrice = selectedShowtime.Movie.moviePrice;
+                    decimal totalAmount = quantity * ticketPrice;
+                    lblTotalAmount.Text = $"Total Amount: Php {totalAmount:F2}";
+                }
         }
-
+        
         private void txtQuantity_TextChanged(object sender, EventArgs e)
         {
             CalculateTotalAmount();
@@ -90,85 +121,64 @@ namespace Dbsys
 
         private void btnCalculateChange_Click(object sender, EventArgs e)
         {
-            // Calculate change based on paid amount
-            if (decimal.TryParse(txtPaidAmount.Text, out decimal paidAmount))
+            if (validation())
             {
-                if (decimal.TryParse(lblTotalAmount.Text.Split(' ')[3], out decimal totalAmount))
+                // Calculate change based on paid amount
+                if (decimal.TryParse(txtPaidAmount.Text, out decimal paidAmount))
                 {
-                    decimal changeAmount = paidAmount - totalAmount;
-                    lblChangeAmount.Text = $"Change: Php {changeAmount:F2}";
+                    if (decimal.TryParse(lblTotalAmount.Text.Split(' ')[3], out decimal totalAmount))
+                    {
+                        decimal changeAmount = paidAmount - totalAmount;
+                        lblChangeAmount.Text = $"Change: Php {changeAmount:F2}";
+                    }
                 }
             }
         }
 
         private void btnConfirmPurchase_Click(object sender, EventArgs e)
         {
-            // Validate inputs
-            if (string.IsNullOrWhiteSpace(txtCustomerName.Text) || string.IsNullOrWhiteSpace(txtQuantity.Text) || string.IsNullOrWhiteSpace(txtPaidAmount.Text))
+            //validation method
+            if (validation())
             {
-                MessageBox.Show("Please fill in all required fields.");
-                return;
-            }
-
-            // Validate quantity is a positive integer
-            if (!int.TryParse(txtQuantity.Text, out int quantity) || quantity <= 0)
-            {
-                MessageBox.Show("Please enter a valid quantity.");
-                return;
-            }
-
-            // Validate paid amount is a positive decimal
-            if (!decimal.TryParse(txtPaidAmount.Text, out decimal paidAmount) || paidAmount < 0)
-            {
-                MessageBox.Show("Please enter a valid paid amount.");
-                return;
-            }
-
-            // Calculate total amount based on ticket price and quantity
-            decimal totalAmount = quantity * selectedShowtime.Movie.moviePrice;
-
-            // Validate paid amount is not less than total amount
-            if (paidAmount < totalAmount)
-            {
-                MessageBox.Show("Paid amount cannot be less than the total amount.");
-                return;
-            }
-
-            decimal changeAmount = paidAmount - totalAmount;
-
-            // Save sales transaction to the database
-            using (var db = new DBSYSEntities())
-            {
-                // Use the stored procedure to insert the sale
-                db.Database.ExecuteSqlCommand(
-                    "sp_InsertSales @ShowtimeID, @CustomerName, @TransactionDate, @MovieID, @SeatNumber, @Quantity, @TotalAmount, @PaidAmount, @ChangeAmount",
-                    new SqlParameter("@ShowtimeID", selectedShowtime.showtimeID),
-                    new SqlParameter("@CustomerName", txtCustomerName.Text),
-                    new SqlParameter("@TransactionDate", DateTime.Now),
-                    new SqlParameter("@MovieID", selectedShowtime.movieID),
-                    new SqlParameter("@SeatNumber", (int)cbSeatNumber.SelectedItem),
-                    new SqlParameter("@Quantity", quantity),
-                    new SqlParameter("@TotalAmount", totalAmount),
-                    new SqlParameter("@PaidAmount", paidAmount),
-                    new SqlParameter("@ChangeAmount", changeAmount)
-                );
-
-                MessageBox.Show("Transaction completed successfully!");
-
-                DialogResult result = MessageBox.Show("Do you want to return to the home page and receive the receipt?", "Confirmation", MessageBoxButtons.YesNo);
-
-                if (result == DialogResult.Yes)
+                // Save sales transaction to the database
+                using (var db = new DBSYSEntities())
                 {
-                    PrintDialog printDialog = new PrintDialog();
-                    if (printDialog.ShowDialog() == DialogResult.OK)
-                    {
-                        printDocument.PrinterSettings = printDialog.PrinterSettings;
-                        printDocument.Print();
-                    }
+                    int quantity = int.Parse(txtQuantity.Text);
+                    decimal totalAmount = quantity * selectedShowtime.Movie.moviePrice;
+                    decimal paidAmount = decimal.Parse(txtPaidAmount.Text);
+                    decimal changeAmount = paidAmount - totalAmount;
 
-                    Frm_CustomerHome h = new Frm_CustomerHome();
-                    h.Show();
-                    this.Close();
+                    // Use the stored procedure to insert the sale
+                    db.Database.ExecuteSqlCommand(
+                        "sp_InsertSales @ShowtimeID, @CustomerName, @TransactionDate, @MovieID, @SeatNumber, @Quantity, @TotalAmount, @PaidAmount, @ChangeAmount",
+                        new SqlParameter("@ShowtimeID", selectedShowtime.showtimeID),
+                        new SqlParameter("@CustomerName", txtCustomerName.Text),
+                        new SqlParameter("@TransactionDate", DateTime.Now),
+                        new SqlParameter("@MovieID", selectedShowtime.movieID),
+                        new SqlParameter("@SeatNumber", (int)cbSeatNumber.SelectedItem),
+                        new SqlParameter("@Quantity", quantity),
+                        new SqlParameter("@TotalAmount", totalAmount),
+                        new SqlParameter("@PaidAmount", paidAmount),
+                        new SqlParameter("@ChangeAmount", changeAmount)
+                    );
+
+                    MessageBox.Show("Transaction completed successfully!");
+
+                    DialogResult result = MessageBox.Show("Do you want to return to the home page and receive the receipt?", "Confirmation", MessageBoxButtons.YesNo);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        PrintDialog printDialog = new PrintDialog();
+                        if (printDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            printDocument.PrinterSettings = printDialog.PrinterSettings;
+                            printDocument.Print();
+                        }
+
+                        Frm_CustomerHome h = new Frm_CustomerHome();
+                        h.Show();
+                        this.Close();
+                    }
                 }
             }
         }
@@ -222,6 +232,7 @@ namespace Dbsys
             e.Graphics.DrawString($"Total Amount: Php {totalAmount:F2}", normalFont, brush, startX, startY);
             startY += lineHeight;
 
+            // Print footer
             e.Graphics.DrawString("==========================", headerFont, brush, startX, startY);
 
             MessageBox.Show("Printed Successfully!", "Sales Receipt", MessageBoxButtons.OK, MessageBoxIcon.Information);
